b6be256b464e416a669a634b39ab8701dd30db0f
fridokus <raxomukus@gmail.com>
2025-03-02 20:24:47 +0100

    fix 's'

--- coinflipper.py
+++ coinflipper.py
@@ -96,7 +96,7 @@ async def flip(update: Update, context: CallbackContext, is_giveflip: bool):
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     msg = await update.message.reply_text(
-        f"{'🎁 Giveflip' if is_giveflip else '🎲 Coinflip'} started! {sats} sats {'given' if is_giveflip else 'entry'}. {n_participants} players needed.",
+        f"{'🎁 Giveflip' if is_giveflip else '🎲 Coinflip'} started! {sats} sats {'given' if is_giveflip else 'entry'}. {n_participants} player{'s' if n_participants > 1 else ''} needed.",
         reply_markup=reply_markup,
     )
 
